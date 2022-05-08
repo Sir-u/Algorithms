f3808ce45b49656451ec280b55f4184424ecb2b5
Siru <ehdgur9553@naver.com>
2022-05-08 14:22:51 +0900

    5/8 END

--- mergesort.c
+++ mergesort.c
@@ -1,68 +1,56 @@
 #include <stdio.h>
 
+// 합병 알고리즘
 void Merge(int S[], int low, int mid, int high)
 {
-    int i, j, k;
-    int U[high];
-    i = low;
-    j = mid + 1;
-    k = low;
-    while (i <= mid && j <= high)
+    int i, j, k, l;
+    int U[high];   // 합병에 필요한 지역 배열
+    i = low;       // i에 index low 저장
+    j = mid + 1;   // j에 index high 저장
+    k = low;       // k에 index low 저장
+
+    while (i <= mid && j <= high)      // i와 j를 비교하여 둘 중 더 작은 값을 U에 저장
     {
         if (S[i] < S[j])
-        {
-            U[k] = S[i];
-            i++;
-        }
+            U[k++] = S[i++];
         else
-        {
-            U[k] = S[j];
-            j++;
-        }
-        k++;
+            U[k++] = S[j++];
     }
 
-    if (i > mid)
-    {
-        for (int l =j; l<=high; l++){
+    if (i > mid)                       // i가 mid를 넘어섰다면 j부터 high까지의 모든 값을 U에 저장
+        for (l =j; l<=high; l++)
             U[k++] = S[l];
-        }
-    }
-    else
-    {
-        for (int l = i; l <= mid; l++)
+    else                               // i부터 mid까지의 값들이 남아있다면 i부터 mid까지의 모든 값을 U에 저장
+        for (l = i; l <= mid; l++)
             U[k++] = S[l];
-    }
 
-    for (int l = low; l <= high; l++)
-    {
+    for (l = low; l <= high; l++)  // U의 값을 S에 저장
         S[l] = U[l];
-    }
 }
 
+// 합병정렬 알고리즘 (분할 + 합병)
 void MergeSort(int S[], int low, int high)
 {
     int mid;
 
     if (low < high)
     {
-        mid = (low + high) / 2;
-        MergeSort(S, low, mid);
-        MergeSort(S, mid + 1, high);
-        Merge(S, low, mid, high);
+        mid = (low + high) / 2;        // mid는 배열의 중앙값
+        MergeSort(S, low, mid);        // mid를 기준으로 low부터 mid까지 합병정렬(분할 및 재귀)
+        MergeSort(S, mid + 1, high);   // mid를 기준으로 mid+1부터 high까지 합병정렬(분할 및 재귀)
+        Merge(S, low, mid, high);      // 분할된 값들을 합병 
     }
 }
 
 int main(void)
 {
     int S[8] = {27, 10, 12, 20, 25, 13, 15, 22};
+    int i;
 
     MergeSort(S, 0, 7);
 
-    for (int i = 0; i < 8; i++)
-    {
+    for (i = 0; i < 8; i++)
         printf("%d ", S[i]);
-    }
 
     return 0;
 }
--- quicksort.c
+++ quicksort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Swap A for B
 void Swap(int *x, int *y) {
     int tmp;
 
@@ -8,44 +9,45 @@ void Swap(int *x, int *y) {
     *y = tmp;
 }
 
+// 분할 알고리즘 (배열을 분할)
 void Partition(int S[], int low, int high, int *pivotpoint)
 {
     int i, j;
-    int pivotitem = S[low];
-    j = low;
+    int pivotitem = S[low];       // pivotitem으로 첫 원소를 선택
+    j = low;                      // low를 j에 저장
 
     for(i = low + 1; i <= high; i++) {
-        if(S[i] < pivotitem){
-            j++;
-            Swap(&S[i], &S[j]);
-        }
-    }
-    *pivotpoint = j;
-    Swap(&S[low], &S[j]);
+        if(S[i] < pivotitem){     // 첫 원소와 비교하여 값의 크기가 작다면
+            j++;                  // j+1을 하여 (j의 초기값 = 배열의 첫원소)
+            Swap(&S[i], &S[j]);   // 비교된 크기가 작은 원소와 교환
+        }                         
+    }                             // i는 모든 원소를 다 순회하고 j는 첫 원소와 비교하여 크기가 작을때마다 1씩 증가하게 됨
+    *pivotpoint = j;              // j로 pivotpoint의 자리를 설정
+    Swap(&S[low], &S[j]);         // 첫 원소와 index j를 교환
 }
 
+// 빠른정렬 알고리즘
 void QuickSort(int S[], int low, int high)
 {
-    int pivotpoint;
+    int pivotpoint; // 기준점
 
     if (high > low)
     {
-        Partition(S, low, high, &pivotpoint);
-        QuickSort(S, low, pivotpoint - 1);
-        QuickSort(S, pivotpoint + 1, high);
+        Partition(S, low, high, &pivotpoint); // pivotitem으로 선택된 원소가 저장되어있는 index가 pivotpoint에 저장
+        QuickSort(S, low, pivotpoint - 1);    // pivotpoint를 기준으로 low부터 pivotpoint-1까지 빠른정렬(분할 및 재귀)
+        QuickSort(S, pivotpoint + 1, high);   // pivotpoint를 기준으로 pivotpoint+1부터 high까지 빠른정렬(분할 및 재귀)
     }
 }
 
 int main(void)
 {
     int S[8] = {15, 22, 13, 27, 12, 10, 20, 25};
-    
+    int i;
+
     QuickSort(S, 0, 7);
 
-    for (int i = 0; i < 8; i++)
-    {
+    for (i = 0; i < 8; i++)
         printf("%d ", S[i]);
-    }
 
     return 0;
 }
